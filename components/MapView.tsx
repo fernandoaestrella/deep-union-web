d9f305ae451e811a3a63194988940810a923812a
Fernando Estrella <fernandoaestrella@gmail.com>
2025-01-04 15:43:17 -0400

    Update MapView.tsx

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -8,7 +8,7 @@ const MapView: React.FC<MapViewProps> = ({ coordinates }) => {
   // Implement map logic here
   return (
     <div>
-      <p>Map view with coordinates: {coordinates}</p>
+      <p>Map view with coordinates: {coordinates}. Here you will be able to see a map of your surroundings, and if others have posted their user data, you will see pins that represent those other people. When you click on those pins, you will be able to see their user data</p>
       {/* Add map implementation here */}
     </div>
   );
