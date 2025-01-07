7b85cb221eda5d6f973cc7d26828d69dd4cc81f4
Fernando Estrella <fernandoaestrella@gmail.com>
2025-01-07 16:52:30 -0400

    add about section
    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.variable}>{children}</body>
+      <body className={inter.variable}>
+        {children}
+        <footer>
+          <div className='text-center'>
+            More experiments <a className='text-slate-400' href="https://github.com/fernandoaestrella">here</a>
+          </div>
+        </footer>
+      </body>
     </html>
   )
 }
+
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { UserData } from '../components/UserDataForm';
 
 // Dynamically import MapView with SSR disabled
 const MapView = dynamic(() => import('../components/MapView'), { ssr: false });
+
 export default function Home() {
   const [userData, setUserData] = useState<UserData | null>(null);
   const [coordinates, setCoordinates] = useState<string>('');
@@ -17,6 +18,7 @@ export default function Home() {
   useEffect(() => {
     setIsClient(true);
   }, []);
+
   const handleUserDataSubmit = (data: UserData) => {
     setUserData(data);
   };
@@ -45,6 +47,23 @@ export default function Home() {
       <section className="mb-8 w-full max-w-2xl">
         <h2 className="mb-4 text-2xl font-semibold">4. Map</h2>
         {isClient && <MapView userData={userData} userCoordinates={coordinates} />}
+      </section>
+
+      <section className="mb-8 w-full max-w-2xl">
+        <h2 className="mb-4 p-6 text-2xl font-semibold">5. About this app</h2>
+        <div className='rounded bg-white p-8 shadow'>
+          <p>
+            We hope this app reminds you of the magic that is already out there in the world. As you use it, we invite you to allow yourself to:
+          </p>
+          <ul className="mt-2 list-disc space-y-2 pl-5">
+            <li>Be clear about your deeper desires instead of being pushed around by expectations.</li>
+            <li>Receive help from someone you may not have considered that could enrich your life.</li>
+            <li>See your life in many more dimensions than just survival and pleasure.</li>
+            <li>Fulfill different needs than the ones you have already covered (i.e. don't continuously fulfill the same one/s).</li>
+            <li>Blur the lines between giving and receiving help, which may help discover a deeper reality than the apparent separation we experience. In other words, be open to what you might receive when you feel you are giving, and open yourself to noticing the flavor your life takes when you feel grateful for the help you receive.</li>
+          </ul>
+          <p className="mt-4">Enjoy!</p>
+        </div>
       </section>
     </main>
   );
