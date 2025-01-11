886d827bea15a703ba319fe93c9641dd5b94cddf
Fernando Estrella <fernandoaestrella@gmail.com>
2025-01-11 16:27:50 -0400

    delete all user data automatically at midnight PST

--- components/PostData.tsx
+++ components/PostData.tsx
@@ -79,6 +79,13 @@ const PostData: React.FC<PostDataProps> = ({ userData, coordinates }) => {
           </pre>
         </div>
 
+        {/* Notify users that their data will be deleted automatically */}
+        <div className="mb-4">
+          <p className="text-gray-600">
+            <b>Please note:</b> All posted data is automatically deleted at midnight Pacific Time every day.
+          </p>
+        </div>
+
         <button 
           className="focus:shadow-outline w-full rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600 focus:outline-none"
           onClick={handlePostData}
